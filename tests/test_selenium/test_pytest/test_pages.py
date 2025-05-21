ad8b26069538885c9e9ce73fb1ffd98f3936acf2
ivan <ivan.zakharov@nordclan.com>
2025-05-21 11:29:55 +0400

    тест со страницами

---
+++ tests/test_selenium/test_pytest/test_pages.py
@@ -0,0 +1,8 @@
+from tests.test_selenium.test_pytest.pages.main_page import MainPage
+from conftest import browser
+
+def test_guest_can_go_to_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    page.open()                      # открываем страницу
+    page.go_to_login_page()
